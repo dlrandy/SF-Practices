630137759a45544816b1531abc660db22a1ee297
dlrandy <1208484996@qq.com>
2021-07-23 11:11:28 +0800

    feat: permutation rereview

--- algorithms/permutation/review/permutateWithRepetitions.js
+++ algorithms/permutation/review/permutateWithRepetitions.js
@@ -2,17 +2,42 @@ export default function permutateWithRepetitions(
   permutationOptions,
   permutationLength = permutationOptions.length
 ) {
-  if (permutationLength == 1) {
-    return permutationOptions.map(permutationOption => [permutationOption]);
+ const permutations  = [];
+ if(permutationLength == 1){
+   // 这里不是这么简单的
+    // return [[permutationOptions[0]]];
+    return permutationOptions.map(permutationOption =>[permutationOption])
   }
-  const permutations = []
-  const smallerPermutations = permutateWithRepetitions(permutationOptions, permutationLength - 1);
-  permutationOptions.forEach( currentPermutationOption => {
-    smallerPermutations.forEach(smallerPermutation => {
-      permutations.push([currentPermutationOption, ...smallerPermutation]);
-    })
-  });
 
-  return permutations;
+  const smallerPermutations = permutateWithRepetitions(permutationOptions, permutationLength - 1);
+  for (let i = 0; i < permutationOptions.length; i++) {
+    const permutationOption = permutationOptions[i];
+    for (let j = 0; j < smallerPermutations.length; j++) {
+      const smallerPermutation = smallerPermutations[j]
+      permutations.push([permutationOption, ...smallerPermutation]);
+    }
+    
+  }
+
+
+ return permutations;
 
 }
+// export default function permutateWithRepetitions(
+//   permutationOptions,
+//   permutationLength = permutationOptions.length
+// ) {
+//   if (permutationLength == 1) {
+//     return permutationOptions.map(permutationOption => [permutationOption]);
+//   }
+//   const permutations = []
+//   const smallerPermutations = permutateWithRepetitions(permutationOptions, permutationLength - 1);
+//   permutationOptions.forEach( currentPermutationOption => {
+//     smallerPermutations.forEach(smallerPermutation => {
+//       permutations.push([currentPermutationOption, ...smallerPermutation]);
+//     })
+//   });
+
+//   return permutations;
+
+// }
--- algorithms/permutation/review/permutateWithoutRepetitions.js
+++ algorithms/permutation/review/permutateWithoutRepetitions.js
@@ -1,28 +1,46 @@
 export default function permutateWithoutRepetitions(permutationOptions) {
-  // 当集合里只有一个数的时候
-  if (permutationOptions.length == 1) {
-    return permutationOptions.map((permutationOption) => [permutationOption]);
-  }
-  //当前集合的最终所有排列
   const permutations = [];
-  // 取出当前集合里的第一个数
   const firstOption = permutationOptions[0];
-  // 对没有第一个数的集合进行排列
-  const smallerPermutations = permutateWithoutRepetitions(
-    permutationOptions.slice(1)
-  );
-  // 遍历拿到的所有子排列
+  if (permutationOptions.length == 1) {
+    return [[firstOption]];
+  }
+  const smallerPermutations = permutateWithoutRepetitions(permutationOptions.slice(1));
   for (let i = 0; i < smallerPermutations.length; i++) {
-    // 取出一个子排列
-    const smallerPermutation = smallerPermutations[i];
-    // 遍历特定的子排列，这个循环实际上做的是将之前的第一个选项
-    // 插入到子排列的不同位置上 (注意这里位置的取值)
-    for (let j = 0; j <= smallerPermutation.length; j++) {
-      const prefix = smallerPermutation.slice(0, j);
-      const suffix = smallerPermutation.slice(j);
+    const permutation = smallerPermutations[i];
+    for (let j = 0; j <= permutation.length; j++) {
+      const prefix = permutation.slice(0,j);
+      const suffix = permutation.slice(j);
       permutations.push([...prefix, firstOption, ...suffix]);
     }
   }
 
   return permutations;
 }
+// export default function permutateWithoutRepetitions(permutationOptions) {
+//   // 当集合里只有一个数的时候
+//   if (permutationOptions.length == 1) {
+//     return permutationOptions.map((permutationOption) => [permutationOption]);
+//   }
+//   //当前集合的最终所有排列
+//   const permutations = [];
+//   // 取出当前集合里的第一个数
+//   const firstOption = permutationOptions[0];
+//   // 对没有第一个数的集合进行排列
+//   const smallerPermutations = permutateWithoutRepetitions(
+//     permutationOptions.slice(1)
+//   );
+//   // 遍历拿到的所有子排列
+//   for (let i = 0; i < smallerPermutations.length; i++) {
+//     // 取出一个子排列
+//     const smallerPermutation = smallerPermutations[i];
+//     // 遍历特定的子排列，这个循环实际上做的是将之前的第一个选项
+//     // 插入到子排列的不同位置上 (注意这里位置的取值)
+//     for (let j = 0; j <= smallerPermutation.length; j++) {
+//       const prefix = smallerPermutation.slice(0, j);
+//       const suffix = smallerPermutation.slice(j);
+//       permutations.push([...prefix, firstOption, ...suffix]);
+//     }
+//   }
+
+//   return permutations;
+// }
